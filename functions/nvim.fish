bc510939ae15d904b5cfe345766e868e7f1f3aef
Derek Stavis <dekestavis@gmail.com>
2024-06-15 14:20:00 -0700

    add --pick option

--- functions/nvim.fish
+++ functions/nvim.fish
@@ -1,11 +1,15 @@
 function nvim -w nvim
   if test -z "$NVIM"; and test -z "$NVIM_LISTEN_ADDRESS"
-    command nvim
-
-    rm -rf $tmpdir
+    command nvim $argv
   else
     if test -z "$argv"
       nvr -s -cc ":enew"
+    else if contains -- "--pick" $argv
+      nvr --remote-send '<C-\><C-n>:set scrollback=1<CR>'
+      set -l bufnr (nvr --remote-expr 'coc_explorer#select_wins#start("ABCDEFGHIJK", [], [], 1)')
+      set -l file (string replace -r -- "--pick" "" $argv)
+      nvr --remote-send ":wincmd $bufnr w<CR>"
+      nvr --remote-send ":e $file<CR>"
     else if test -f "$argv"
       nvr -s --remote-wait-silent "$argv"
     else
